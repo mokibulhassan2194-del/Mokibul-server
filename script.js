5e2be599c3299b8c5ecb8f598e448961aa1a092f
mokibulhassan2194-del <mokibulhassan2194@gmail.com>
2026-04-22 15:35:24 +0530

    Add scroll event to play/pause videos in view

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const wrapper = document.getElementById('mainContainer');
+if (wrapper) {
+    wrapper.addEventListener('scroll', () => {
+        const vids = document.querySelectorAll('video');
+        vids.forEach(v => {
+            const rect = v.getBoundingClientRect();
+            if (rect.top >= 0 && rect.top < window.innerHeight / 2) {
+                v.play().catch(() => {});
+            } else {
+                v.pause();
+            }
+        });
+    }, { passive: true });
+}
 
